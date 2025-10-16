39209ee0d91adb988f044c30bb06c855545b6dbc
OnlyAHuman7926 <ryanj_2019@163.com>
2025-10-16 22:30:32 +0800

    updated assets

--- assets/Click the Beans_files/undrag.js
+++
@@ -1,8 +0,0 @@
-let mousedownTagName = "";
-document.addEventListener("mousedown", e => {
-  mousedownTagName = e.target.tagName.toUpperCase();
-}, false);
-document.addEventListener("mouseup", e => {
-  mousedownTagName = "";
-}, false);
-window.ondragstart = () => mousedownTagName != "IMG";
